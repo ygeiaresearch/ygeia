9bd301bcd264912a3d8f25bfbf14e126b3c73a96
Michael Johnson <self@mikejohnsonjr.com>
2023-08-27 12:39:52 -0500

    front page

--- root_app/views.py
+++ root_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+	context = {}
+	template_name = 'home.html'
+	return render(request, template_name, context)
+
+def about(request):
+	context = {}
+	template_name = 'about.html'
+	return render(request, template_name, context)

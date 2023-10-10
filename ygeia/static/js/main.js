b93069c789d71089eb68b6998922af6912d00dac
Michael Johnson <self@mikejohnsonjr.com>
2023-10-10 13:02:30 +0200

    console log amounts for debugging

--- ygeia/static/js/main.js
+++ ygeia/static/js/main.js
@@ -1,7 +1,7 @@
 Ygeia = {
 	calculate_amt_pot_perm : function(mn_amt, fe_amt){
 		console.log('Mn: '+ mn_amt);
-		console.log('Fe: '+ fn_amt);
+		console.log('Fe: '+ fe_amt);
 		return ((parseFloat(mn_amt) * 1.92) + (parseFloat(fe_amt) + 0.57))
 	}
 }

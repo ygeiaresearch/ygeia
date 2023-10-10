fea72db1e9302d975938d2c8eedbd04a197dc56b
Michael Johnson <self@mikejohnsonjr.com>
2023-10-10 12:43:04 +0200

    update potperm formula

--- ygeia/static/js/main.js
+++ ygeia/static/js/main.js
@@ -1,5 +1,5 @@
 Ygeia = {
 	calculate_amt_pot_perm : function(fe_amt, mn_amt){
-		return (parseFloat(fe_amt) + parseFloat(mn_amt) * 3)
+		return ((parseFloat(mn_amt) * 1.92) + (parseFloat(fe_amt) + 0.57))
 	}
 }

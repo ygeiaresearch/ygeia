6a26ce66a36f322cc8c7b80d049daa103b4f3429
Michael Johnson <self@mikejohnsonjr.com>
2023-10-10 12:54:22 +0200

    keydown not keyup

--- ygeia/static/js/main.js
+++ ygeia/static/js/main.js
@@ -1,5 +1,5 @@
 Ygeia = {
-	calculate_amt_pot_perm : function(fe_amt, mn_amt){
+	calculate_amt_pot_perm : function(mn_amt, fe_amt){
 		return ((parseFloat(mn_amt) * 1.92) + (parseFloat(fe_amt) + 0.57))
 	}
 }
